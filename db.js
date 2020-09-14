25593f9198b07b35b6c77fae632ea950ff955d73
Kim <kim@bylr.ch>
2020-09-14 22:55:13 +0200

    update dependencies

--- db.js
+++ db.js
@@ -5,9 +5,10 @@ mongoose.Promise = global.Promise;
 // mongoose.connect('mongodb://localhost:27017/todo', {
 //   useMongoClient: true,
 // });
-mongoose.connect(`mongodb://${ process.env.DB_USER }:${ process.env.DB_PASS }@${ process.env.DB_HOST }`, {
-  useMongoClient: true,
-});
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}`,
+  { useUnifiedTopology: true, useNewUrlParser: true },
+);
 
 const db = mongoose.connection;
 
